9cf7eda8fe9b42ecce90a52cfbcb0245a3ee04eb
Allisson Matheus <allisson.boiane@outlook.com>
2023-04-25 10:21:20 -0300

    Desafio Dois

---
+++ js/desafioDois.js
@@ -0,0 +1,53 @@
+// ------------------------------ Desafio Dois -------------------------------------------
+
+const RESPOSTA = document.getElementById("respostaDesafioDois");
+
+const INPUT = document.getElementById("numero");
+
+const BT = document.getElementById("btDesafioDois");
+BT.addEventListener("click", verficaNumero)
+
+function verficaNumero () {
+
+    const NUMERO = INPUT.value
+
+    if (NUMERO == "") {
+        alert("Informe um número no Desafio Dois!!")
+        return
+    }
+
+    fibonacci(NUMERO);
+
+    console.log("Desafio Dois: " + fibonacci(NUMERO));
+
+    INPUT.value = "";
+    RESPOSTA.textContent = fibonacci(NUMERO);
+
+}
+
+function fibonacci(NUMERO) {
+
+    let seqFibo = 0;
+    
+    let num01 = 0;
+    let num02 = 1;
+    
+    
+    for (let i = 0; i <= NUMERO; i++) {
+
+        if(NUMERO == seqFibo) {
+
+            return `O número ${NUMERO} pertence a sequência de Fibonacci`
+
+        } else if (seqFibo > NUMERO) {
+            
+            return `O número ${NUMERO} não pertence a sequência de Fibonacci`
+        }
+    
+        seqFibo = num01 + num02;
+    
+        num01 = num02;
+        num02 = seqFibo;
+    }
+}
+
---
+++ js/desafioUm.js
@@ -0,0 +1,15 @@
+// ------------------------------ Desafio Um -------------------------------------------
+
+const INDICE = 13;
+
+var soma = 0;
+var k = 0;
+
+while (k < INDICE) {
+
+    k = k + 1;
+    soma = soma + k;
+
+}
+
+console.log("Desafio um: " + soma);
